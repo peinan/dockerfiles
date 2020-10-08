0094d6573ba237e5949b0f49b950628d973cac3c
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2020-10-08 12:07:26 +0900

    updated dependencies and configs

--- nlp-jp-gpu/install_jupyterlab_extensions.sh
+++ nlp-jp-gpu/install_jupyterlab_extensions.sh
@@ -2,11 +2,6 @@
 
 jupyter labextension install \
     @lckr/jupyterlab_variableinspector \
-    @jupyterlab/plotly-extension \
     @jupyterlab/toc \
-    @jupyterlab/katex-extension \
-    #jupyterlab_tensorboard \
-    #@jupyter-widgets/jupyterlab-manager@0.38 \
-    #@krassowski/jupyterlab_go_to_definition
-#jupyter serverextension enable --py jupyterlab_tensorboard
-#jupyter serverextension enable --py --sys-prefix widgetsnbextension
+    @jupyter-widgets/jupyterlab-manager
+    jupyterlab-plotly@4.11.0
