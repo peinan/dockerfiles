84bd1caa8326cfd3428d9189500ebc4fc201f927
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2020-10-20 00:20:33 +0900

    added execute time plugin to jupyter lab

--- nlp-jp-gpu/install_jupyterlab_extensions.sh
+++ nlp-jp-gpu/install_jupyterlab_extensions.sh
@@ -4,4 +4,6 @@ jupyter labextension install \
     @lckr/jupyterlab_variableinspector \
     @jupyterlab/toc \
     @jupyter-widgets/jupyterlab-manager \
-    jupyterlab-plotly@4.11.0
+    jupyterlab-plotly@4.11.0 \
+    jupyterlab-execute-time
+
